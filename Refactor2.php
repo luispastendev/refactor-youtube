a5abeadd9f6eb6b0bba4f5debb8d8f2e7e1597dc
Luis Pastén <luispasten.dev@gmail.com>
2021-09-14 22:23:17 -0500

    texto corregido

--- Refactor2.php
+++ Refactor2.php
@@ -59,6 +59,70 @@ class User implements SplSubject {
 
 } 
 
+class EmailSender implements SplObserver { 
 
-$o = new User('luis@gmail.com', 'lpndev', '5512121212','shef');
-$o->create();
+   public function update(SplSubject $subject) { 
+       if (func_num_args() === 2) { 
+           $userInfo = func_get_arg(1); 
+           echo "Se envió correctamente un correo electrónico a {$userInfo['email']}. El contenido es: Hola {$userInfo['username']}". 
+                        "Su nueva contraseña es {$userInfo['password']}, manténgala segura.", PHP_EOL; 
+       } 
+   } 
+
+} 
+
+
+class MobileSender implements SplObserver { 
+
+    public function update(SplSubject $subject) { 
+        if (func_num_args() === 2) { 
+            $userInfo = func_get_arg(1); 
+            echo "Se envió correctamente un mobile a {$userInfo['mobile']}. El contenido es: Hola {$userInfo['username']}". 
+                         "Su nueva contraseña es {$userInfo['password']}, manténgala segura", PHP_EOL; 
+        } 
+    } 
+
+} 
+
+class WebsiteSender implements SplObserver { 
+
+    public function update(SplSubject $subject){
+        if (func_num_args() === 2) { 
+            $userInfo = func_get_arg(1); 
+            echo "Se cambio la password para la cuenta {$userInfo['email']}". 
+                    "Su nueva contraseña es {$userInfo['password']}, manténgala segura", PHP_EOL; 
+        } 
+    }
+}
+ 
+ 
+
+// header('Content-Type: text/plain'); 
+
+// function __autoload($class_name) { 
+//    require_once "$class_name.php"; 
+// } 
+
+$email_sender = new EmailSender(); 
+$mobile_sender = new MobileSender(); 
+$web_sender = new WebsiteSender(); 
+
+$user = new User('usuario1@dominio.com', 'Zhang San', '13610002000', '123456'); 
+
+  // Notificar a los usuarios por correo electrónico y SMS al crear usuarios
+// $user->attach($email_sender); 
+// $user->attach($mobile_sender); 
+// $user->create($user); 
+// echo PHP_EOL; 
+
+//   // Si el usuario restablece la contraseña después de olvidar la contraseña, debe notificar al usuario a través de una pequeña nota
+$user->attach($web_sender); 
+$user->attach($email_sender); 
+$user->resetPassword(); 
+echo PHP_EOL; 
+
+//   // El usuario ha cambiado la contraseña, pero no envía mensajes de texto a su teléfono móvil
+// $user->detach($mobile_sender); 
+// $user->changePassword('654321'); 
+// echo PHP_EOL; 
+
